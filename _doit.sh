ccd413589f90b718b89e9f242df67a25e4a2b793
root <root@backupche>
2020-10-19 22:49:24 +0200

    xx

---
+++ _doit.sh
@@ -0,0 +1,11 @@
+@echo off
+
+rm profile.tar profile.tar.bz2
+chmod 755 bash_profile bashrc crs_stat-t
+chmod 644 wynik.txt
+
+tar cvf profile.tar bash_profile bashrc test.sh crs_stat-t wynik.txt pickora .pgm-boundle-version dfc login.sql i.sql
+
+ls -l
+bzip2 -9 profile.tar
+uuencode profile.tar.bz2 profile.tar.bz2 | unix2dos > profile.tar.bz2.uue
