324b62d1e81d042d6b12728be64e307c27470138
root <root@yt2podcast.eth.che.matuszyk.com>
2021-01-30 16:06:45 +0100

    "new push from yt2podcast.eth.che.matuszyk.com @ 2021.01.30 16:06:45"

--- git-push.sh
+++ git-push.sh
@@ -1,8 +1,11 @@
+# 2021.01.30 - v. 0.4 - making this script more generic (by adding github_project_name variable)
 # 2020.11.27 - v. 0.3 - changed rm to remove files from the directory not the whole directory
 # 2020.11.26 - v. 0.2 - added second section with 'git pull'
 # 2020.10.20 - v. 0.1 - initial release
 
-github_project_name=github-bash_profile
+github_project_name=`pwd`
+github_project_name=`basename $github_project_name`
+echo $github_project_name
 
 echo "Do you want to do git push? [y/N]"
 read -t 5 -n 1 p     # read one character (-n) with timeout of 5 seconds
