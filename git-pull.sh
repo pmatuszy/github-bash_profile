1b050db16c723532c91de31fe931772e7de788e7
root <matuszyk@matuszyk.com>
2023-02-17 00:31:48 +0100

    "new push from nuci7 @ 2023.02.17 00:31:48"

--- git-pull.sh
+++ git-pull.sh
@@ -9,8 +9,13 @@
 export GIT_REPO_DIRECTORY=/root/github-bash_profile
 export github_project_name=github-bash_profile
 
-check_if_installed keychain
-keychain --nocolor id_ed25519 id_SSH_ed25519_20230207_OpenSSH
+export GIT_SSH_COMMAND='ssh -i $HOME/.ssh/id_SSH_ed25519_20230207_OpenSSH'
+
+eval keychain -q --nogui --nocolor --eval id_rsa id_ed25519 id_SSH_ed25519_20230207_OpenSSH 2>&1
+
+if [ -f $HOME/.keychain/$HOSTNAME-sh ];then
+  . $HOME/.keychain/$HOSTNAME-sh
+fi
 
 batch_mode=0
 
