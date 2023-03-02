c892227bc2c6e8ffd0203cf61a8f6a26f884f835
root <pmatuszy.login+github@matuszyk.com>
2023-03-02 12:44:00 +0100

    "new push from backupss.eth.ss.matuszyk.com @ 2023.03.02 12:44:00"

--- _doit.sh
+++ _doit.sh
@@ -9,16 +9,3 @@ tar cvf profile.tar bash_profile bashrc install.sh crs_stat-t wynik.txt pickora 
 ls -l
 bzip2 -9 profile.tar
 uuencode profile.tar.bz2 profile.tar.bz2 | unix2dos > profile.tar.bz2.uue
-
-echo now:
-echo "git add * .pgm-boundle-version"
-echo git commit -m \""new push from _doit.sh (`hostname`) @ `date '+%Y.%m.%d %H:%M:%S'`"\"
-echo "git push"
-
-echo "DEBUG: do you want to do git push? [y/N]"
-read -t 5 -n 1 p     # read one character (-n) with timeout of 5 seconds
-if [ "${p}" == 'y' -o  "${p}" == 'y' ]; then
-  git add *
-  git commit -m \""new push from _doit.sh (`hostname`) @ `date '+%Y.%m.%d %H:%M:%S'`"\"
-  git push
-fi 
